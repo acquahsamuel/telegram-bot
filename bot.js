83e1508e2e3618a5be2a3639b945788b4b2d26f1
acquahsamuel <acquahsamuel1996@gmail.com>
2020-07-21 03:11:15 +0000

    git ignore fix

--- bot.js
+++ bot.js
@@ -1,22 +1,16 @@
 const  Telegraf  = require('telegraf');
 
 const dotenv = require("dotenv");
-dotenv.config({ path: './config.env' })
+dotenv.config({ path: './.env' })
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-
-bot.start((ctx) => ctx.reply('Welcome'))
-bot.help((ctx) => ctx.reply('Send me a sticker'))
-
-bot.on('sticker', (ctx) => ctx.reply('👍'))
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
 
 bot.command('oldschool', (ctx) => ctx.reply('Hello'))
 bot.command('modern', ({ reply }) => reply('Yo'))
 bot.command('hipster', Telegraf.reply('λ'))
 
-bot.launch()
+bot.launch();
 
 
 
